dfd663f61ef69a6f5014a66e5af2f35697c9a91f
DESKTOP-3FG302D\darkb <Johnny Rangel>
2022-06-08 01:24:26 -0700

    - Experimenting with Fetch function

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../App.module.css'
 
 function Searchbar() {
+  let [term, setTerm] = useState('')
+
+
+    function userInput(event: React.ChangeEvent<HTMLInputElement>) {
+      setTerm(term = event.target.value)
+    }
 
     function clicked(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
-        console.log('bruh')
+        fetch("/CITYLIST_test.json")
+        .then(response => response.json()).then(data => {
+          console.log(data[0].name)
+        })
     }
 
   return (
     <div>
         <form onSubmit={clicked}>
             <label className={style.label}>Search Bar:</label>
-            <input className={style.search_bar}></input>
+            <input className={style.search_bar} value={term} onChange={userInput}></input>
         </form>
     </div>
   );
