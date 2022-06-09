b4b485c7afc64847c86c05f1050bbabcdf1205be
DESKTOP-3FG302D\darkb <Johnny Rangel>
2022-06-09 01:26:08 -0700

    - Changing, Improving, Expirementing

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,27 +1,41 @@
 import React, { useState } from 'react';
 import style from '../App.module.css'
 
+// type defines JSON data being retrieved
+interface city {
+  coord: {lon: number, lat: number}
+  country: string
+  id: number
+  name: string
+  state: string
+}
+
 function Searchbar() {
-  let [term, setTerm] = useState('')
+  let [input, setInput] = useState('')
 
 
-    function userInput(event: React.ChangeEvent<HTMLInputElement>) {
-      setTerm(term = event.target.value)
+    function changeInput(event: React.ChangeEvent<HTMLInputElement>) {
+      setInput(input = event.target.value)
     }
 
-    function clicked(event: React.FormEvent<HTMLFormElement>) {
+    function searchInput(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault()
         fetch("/CITYLIST_test.json")
-        .then(response => response.json()).then(data => {
-          console.log(data[0].name)
+        .then(response => response.json()).then((data: Array<city>) => {
+          data.forEach(city => {
+            if(city.name === input) {
+              return console.log(city.name)
+            }
+
+          })
         })
     }
 
   return (
     <div>
-        <form onSubmit={clicked}>
+        <form onSubmit={searchInput}>
             <label className={style.label}>Search Bar:</label>
-            <input className={style.search_bar} value={term} onChange={userInput}></input>
+            <input className={style.search_bar} value={input} onChange={changeInput} placeholder="City"></input>
         </form>
     </div>
   );
